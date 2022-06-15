ce30e52e3e8f2057a4416428a573ae1d0b8f5b67
DavidOriginalnoeImya <abulashvili@inbox.ru>
2022-06-15 21:10:28 +0300

    edit trade service

--- trade-service/src/main/java/ru/home/rest/UserInfoREST.java
+++ trade-service/src/main/java/ru/home/rest/UserInfoREST.java
@@ -1,6 +1,9 @@
 package ru.home.rest;
 
+import io.quarkus.oidc.IdToken;
+import io.quarkus.security.identity.SecurityIdentity;
 import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.jboss.logging.Logger;
 import ru.home.dto.UserFunctionDTO;
 
 import javax.inject.Inject;
@@ -10,43 +13,42 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
-import java.util.logging.Logger;
 
 @Path("/api/user")
 public class UserInfoREST {
     private static final Logger LOGGER = Logger.getLogger(UserInfoREST.class.getSimpleName());
 
     private static final List<UserFunctionDTO> storeWorkerFunctions = List.of(
-            new UserFunctionDTO().setFunctionName("Прием товара в магазин").setFunctionUri(""),
-            new UserFunctionDTO().setFunctionName("Продажа товара").setFunctionUri(""),
-            new UserFunctionDTO().setFunctionName("Заказ товара со склада").setFunctionUri(""),
-            new UserFunctionDTO().setFunctionName("Получение справки о товаре").setFunctionUri("")
+            new UserFunctionDTO().setFunctionName("Прием товара в магазин").setFunctionUri(" "),
+            new UserFunctionDTO().setFunctionName("Продажа товара").setFunctionUri(" "),
+            new UserFunctionDTO().setFunctionName("Заказ товара со склада").setFunctionUri(" "),
+            new UserFunctionDTO().setFunctionName("Получение справки о товаре").setFunctionUri(" ")
     );
 
     private static final List<UserFunctionDTO> storeKeeperFunctions = List.of(
-            new UserFunctionDTO().setFunctionName("Список активных заявок").setFunctionUri(""),
-            new UserFunctionDTO().setFunctionName("Прием товара на склад").setFunctionUri(""),
-            new UserFunctionDTO().setFunctionName("Отпуск товара в магазин").setFunctionUri(""),
-            new UserFunctionDTO().setFunctionName("Получение справки о товаре").setFunctionUri("")
+            new UserFunctionDTO().setFunctionName("Список активных заявок").setFunctionUri(" "),
+            new UserFunctionDTO().setFunctionName("Прием товара на склад").setFunctionUri(" "),
+            new UserFunctionDTO().setFunctionName("Отпуск товара в магазин").setFunctionUri(" "),
+            new UserFunctionDTO().setFunctionName("Получение справки о товаре").setFunctionUri(" ")
     );
 
+//    @Inject
+//    JsonWebToken jwt;
+
     @Inject
-    JsonWebToken jwt;
+    SecurityIdentity identity;
 
     @GET
     @Path("/functions")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getUserFunctionByRole() {
-        if (jwt != null) {
-            if (jwt.getGroups().contains("storeworker")) {
-                return Response.ok(storeWorkerFunctions).build();
-            }
-            else if (jwt.getGroups().contains("storekeeper")) {
-                return Response.ok(storeKeeperFunctions).build();
-            }
-            else return Response.status(425, "Unknown role").build();
+        if (identity.hasRole("storeworker")) {
+            return Response.ok(storeWorkerFunctions).build();
+        }
+        else if (identity.hasRole("storekeeper")) {
+            return Response.ok(storeKeeperFunctions).build();
         }
 
-        return Response.status(426, "Invalid token").build();
+        return Response.status(425, "Unknown role").build();
     }
 }
